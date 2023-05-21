0818b4985e5e82bdfb8d9d14af2a9505574d2de6
Vladislav <VladikaSITX@yandex.ru>
2023-05-21 21:46:04 +0300

    1

---
+++ colculator/src/Calculate.java
@@ -0,0 +1,14 @@
+public class Calculate {
+    public double addition(double a, double b){
+        return a+b;
+    }
+    public double subtraction(double a, double b){
+        return a-b;
+    }
+    public double division(double a, double b){
+        return a/b ;
+    }
+    public double multiplication(double a, double b){
+        return a*b;
+    }
+}
---
+++ colculator/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Calculate calculate = new Calculate();
+        System.out.println(calculate.addition(2,5));
+        System.out.println(calculate.subtraction(2,5));
+        System.out.println(calculate.multiplication(2,5));
+        System.out.println(calculate.division(2,5));
+    }
+}
